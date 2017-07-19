9816dbd7c548f7c32b087a6919b3b16b1521cf72
Darko Prince Yanney <darkoprinceyanney45@gmail.com>
2017-07-19 11:32:19 +0000

    notification

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,11 +1,68 @@
+from gpiozero import LED
+from time import sleep
 import paho.mqtt.client as mqtt
 
+
+
+led = LED(17)
+
+
+def dot():
+        led.on()
+        sleep(0.1)
+        led.off()
+        sleep(0.1)
+
+def dash():
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
+     
+
+def p():
+        dot()
+        dash()
+        dash()
+        dot()
+
+def r():
+        dot()
+        dash()
+        dot()
+def i():
+        dot()
+        dot()
+def n():
+        dash()
+        dot()
+def c():
+        dash()
+        dot()
+        dash()
+        dot()
+def e():
+        dot()
+
+
+
+def say_my_name():
+        p()
+        r()
+        i()
+        n()
+        c()
+        e()
+
 def on_message(client, userdata, message):
-	print message.payload
+        print message.payload
+	say_my_name()
+	
 
 def on_connect(client, userdata, flags, code):
-	print "connected:" + str(code)
-	client.subscribe("test/all")
+        print "connected:" + str(code)
+        client.subscribe("test/all")
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -14,3 +71,4 @@ client.on_message = on_message
 client.connect('moorhouseassociates.com', 1883, 60)
 
 client.loop_forever()
+
